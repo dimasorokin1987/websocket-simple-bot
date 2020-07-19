13f74845decdc41e2f0ed31484f5aee378f4a9b8
dimasorokin1987 <58263696+dimasorokin1987@users.noreply.github.com>
2020-07-19 21:13:13 +0300

    Update websocket-simple-bot.go
    remove commented stuff and small refactoring

--- websocket-simple-bot.go
+++ websocket-simple-bot.go
@@ -3,7 +3,6 @@ package main
 import (
   "fmt"
   "os"
-  //"io"
   "net/http"
   "log"
   "golang.org/x/net/websocket"
@@ -15,28 +14,20 @@ const (
 
 type T struct {
   Txt string `json:"text"`
- // Msg string
- // Count int
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "Hello World!")
 }
 
-func processRequest(ws *websocket.Conn){
-  data := T{}
-  err := websocket.JSON.Receive(ws, &data) 
-  if err != nil {
-    log.Fatalln("error receiving json")
-  }
-  websocket.JSON.Send(ws, data)
-}
-
-// Echo the data received on the WebSocket. 
-func EchoServer(ws *websocket.Conn) {
-  //io.Copy(ws, ws)
+func WebsocketServer(ws *websocket.Conn) {
   for {
-    processRequest(ws)
+    data := T{}
+    err := websocket.JSON.Receive(ws, &data) 
+    if err != nil {
+      log.Fatalln("error receiving json")
+    }
+    websocket.JSON.Send(ws, data)
   }
 }
 
@@ -45,19 +36,10 @@ func main() {
   mux := http.NewServeMux()
   mux.Handle("/", http.FileServer(http.Dir(directory)))
   mux.Handle("/test", http.HandlerFunc(indexHandler))
-  mux.Handle("/ws", websocket.Handler(EchoServer))
+  mux.Handle("/ws", websocket.Handler(WebsocketServer))
   s := http.Server{Addr: ":" + port, Handler: mux}
   err := s.ListenAndServe()
   if err != nil {
-    log.Fatalln("ListenAndServe: " + err.Error())
+    log.Fatalln("ListenAndServe: " + err.Error)
   }
-
-/*http.Handle("/", http.FileServer(http.Dir(directory)))
-  http.Handle("/ws", websocket.Handler(EchoServer))
-  // err := http.ListenAndServe(":12345", nil)
-  http.Handle("/test", http.HandlerFunc(indexHandler))
-  err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-  if err != nil {
-    log.Fatalln("ListenAndServe: " + err.Error())
-  }*/
 }
